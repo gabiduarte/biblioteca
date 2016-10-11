d48c152eb517e80cc42b373180453fdd9eb15bf4
Gabrielle Saman <gsaman@thoughtworks.com>
2016-10-11 12:08:44 -0300

    Fix #8 - New success message when checking out book

--- src/com/twu/biblioteca/UIStrings.java
+++ src/com/twu/biblioteca/UIStrings.java
@@ -8,7 +8,7 @@ public class UIStrings {
     public static final String LIBRARY_BOOKS_AVAILABLE = "Books Available:" + LINE_BREAK + LINE_BREAK;
     public static final String LIBRARY_BOOKS_UNAVAILABLE = "No available books in Biblioteca";
     public static final String CHECKOUT_MENU = "Choose the ID of the book you want to checkout";
-    public static final String CHECKOUT_SUCCESS = "Enjoy your book";
+    public static final String CHECKOUT_SUCCESS = "Thank you! Enjoy the book";
 
     public static final String MENU_OPTIONS = LINE_BREAK +
             "Choose from the option below:" + LINE_BREAK  +
